4998980c957836eba2431edd1596684eaa9e717c
Alex Riviere <alexanderm.riviere@gmail.com>
2021-11-01 10:06:15 +0000

    lesson5 started

--- lesson5/sorting.rb
+++ lesson5/sorting.rb
@@ -3,3 +3,7 @@ p ['arc', 'bat', 'cape', 'ants', 'cap'].sort
 #guess ['arc', 'ants', 'bat', 'cap', 'cape']
 #ants before arc
 
+[['a', 'cat', 'b', 'c'], ['b', 2], ['a', 'car', 'd', 3], ['a', 'car', 'd']].sort
+
+#guess
+
